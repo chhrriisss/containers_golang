28b56ed1fa873bff2d412f28f4e66ea729375a04
Chris <chrisbenny723@gmail.com>
2025-07-14 10:31:02 +0000

    Update: modify main.go to fix pivot_root and container PID handling

--- main.go
+++ main.go
@@ -1,59 +1,62 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"strconv"
-	"syscall"
+    "fmt"
+    "os"
+    "os/exec"
+    "path/filepath"
+    "syscall"
 )
 
 func main() {
-	switch os.Args[1] {
-	case "run":
-		run()
-	case "child":
-		child()
-	default:
-		panic("bad command")
-	}
+    switch os.Args[1] {
+    case "run":
+        run()
+    case "child":
+        child()
+    default:
+        panic("Unknown command")
+    }
 }
 
 func run() {
-	fmt.Printf("Running %v as PID %d\n", os.Args[2:], os.Getpid())
+    cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+    cmd.Stdin = os.Stdin
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
 
-	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+    cmd.SysProcAttr = &syscall.SysProcAttr{
+        Cloneflags: syscall.CLONE_NEWUTS |
+            syscall.CLONE_NEWPID |
+            syscall.CLONE_NEWNS |
+            syscall.CLONE_NEWNET |
+            syscall.CLONE_NEWIPC,
+    }
 
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS |
-			syscall.CLONE_NEWPID |
-			syscall.CLONE_NEWNS,
-		Unshareflags: syscall.CLONE_NEWNS,
-	}
-
-	must(cmd.Run())
+    must(cmd.Run())
 }
 
 func child() {
     fmt.Printf("Running %v as PID %d\n", os.Args[2:], os.Getpid())
-    must(syscall.Sethostname([]byte("container")))
 
-    must(os.MkdirAll("/proc", 0755))
-    _ = syscall.Unmount("/proc", 0)
-    must(syscall.Mount("proc", "/proc", "proc", 0, ""))
+    rootfs := "/home/chris/containers-from-scratch/rootfs"
 
-    must(os.MkdirAll("/sys/fs/cgroup", 0755))
-    _ = syscall.Unmount("/sys/fs/cgroup", 0)
-    must(syscall.Mount("none", "/sys/fs/cgroup", "cgroup2", 0, ""))
+    must(syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, ""))
 
-    cg()
+    must(os.MkdirAll(filepath.Join(rootfs, "proc"), 0755))
+    must(os.MkdirAll(filepath.Join(rootfs, "old_root"), 0755))
 
-    must(syscall.Chroot("./rootfs"))
-    must(os.Chdir("/"))
+    // Make sure rootfs is a mount point
+    must(syscall.Mount(rootfs, rootfs, "", syscall.MS_BIND|syscall.MS_REC, ""))
+
+    must(syscall.Mount("proc", filepath.Join(rootfs, "proc"), "proc", 0, ""))
+
+    must(syscall.Chdir(rootfs))
+    must(syscall.PivotRoot(".", "old_root"))
+    must(syscall.Chdir("/"))
+
+    must(syscall.Unmount("/old_root", syscall.MNT_DETACH))
+    must(os.Remove("/old_root"))
 
     cmd := exec.Command(os.Args[2], os.Args[3:]...)
     cmd.Stdin = os.Stdin
@@ -61,23 +64,10 @@ func child() {
     cmd.Stderr = os.Stderr
 
     must(cmd.Run())
-    _ = syscall.Unmount("/proc", 0)
 }
-
-func cg() {
-    cgroups := "/sys/fs/cgroup"
-    containerCgroup := filepath.Join(cgroups, "liz")
-
-    must(os.MkdirAll(containerCgroup, 0755))
-
-    must(os.WriteFile(filepath.Join(containerCgroup, "memory.max"), []byte("100000000"), 0700))
-    must(os.WriteFile(filepath.Join(containerCgroup, "pids.max"), []byte("20"), 0700))
-    must(os.WriteFile(filepath.Join(containerCgroup, "cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
-}
-
 
 func must(err error) {
-	if err != nil {
-		panic(err)
-	}
+    if err != nil {
+        panic(err)
+    }
 }
