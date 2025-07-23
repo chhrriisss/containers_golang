ee1b093496fc93f10a77aaea9b9ca85b428ae880
Chris <chrisbenny723@gmail.com>
2025-07-23 09:23:00 +0000

    Add network connectivity support to container runtime

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "log"
     "os"
     "os/exec"
     "path/filepath"
@@ -24,7 +25,6 @@ func run() {
     cmd.Stdin = os.Stdin
     cmd.Stdout = os.Stdout
     cmd.Stderr = os.Stderr
-
     cmd.SysProcAttr = &syscall.SysProcAttr{
         Cloneflags: syscall.CLONE_NEWUTS |
             syscall.CLONE_NEWPID |
@@ -33,74 +33,81 @@ func run() {
             syscall.CLONE_NEWIPC,
     }
 
-    // Step 1: Start the container process
-    must(cmd.Start())
+    must(cmd.Start(), "starting child container process")
     fmt.Printf("Container PID: %d\n", cmd.Process.Pid)
 
-    // Step 2: Set up networking
     setupNetwork(cmd.Process.Pid)
-
-    // Step 3: Wait for the container to exit
-    must(cmd.Wait())
+    must(cmd.Wait(), "waiting for container")
 }
 
 func setupNetwork(pid int) {
     fmt.Println("Setting up veth pair...")
 
-    // Create veth pair
-    must(exec.Command("ip", "link", "add", "veth0", "type", "veth", "peer", "name", "veth1").Run())
+    fmt.Println("-> Creating veth pair")
+    must(exec.Command("ip", "link", "add", "veth0", "type", "veth", "peer", "name", "veth1").Run(), "create veth pair")
 
-    // Move veth1 to the container's network namespace
-    must(exec.Command("ip", "link", "set", "veth1", "netns", fmt.Sprint(pid)).Run())
+    fmt.Println("-> Moving veth1 to container netns")
+    must(exec.Command("ip", "link", "set", "veth1", "netns", fmt.Sprint(pid)).Run(), "move veth1")
 
-    // Setup host end: veth0
-    must(exec.Command("ip", "addr", "add", "10.0.0.1/24", "dev", "veth0").Run())
-    must(exec.Command("ip", "link", "set", "veth0", "up").Run())
+    fmt.Println("-> Assigning IP to veth0")
+    must(exec.Command("ip", "addr", "add", "10.0.0.1/24", "dev", "veth0").Run(), "assign IP to veth0")
 
-    // Setup container end: veth1 (inside netns)
-    must(exec.Command("nsenter", "-t", fmt.Sprint(pid), "-n",
-        "ip", "addr", "add", "10.0.0.2/24", "dev", "veth1").Run())
-    must(exec.Command("nsenter", "-t", fmt.Sprint(pid), "-n",
-        "ip", "link", "set", "veth1", "up").Run())
-    must(exec.Command("nsenter", "-t", fmt.Sprint(pid), "-n",
-        "ip", "link", "set", "lo", "up").Run())
+    fmt.Println("-> Bringing up veth0")
+    must(exec.Command("ip", "link", "set", "veth0", "up").Run(), "bring up veth0")
 
-    fmt.Println(" Network setup complete")
+    fmt.Println("-> Assigning IP to veth1 (container side)")
+    must(exec.Command("nsenter", "-t", fmt.Sprint(pid), "-n", "ip", "addr", "add", "10.0.0.2/24", "dev", "veth1").Run(), "assign IP to veth1")
+
+    fmt.Println("-> Bringing up veth1")
+    must(exec.Command("nsenter", "-t", fmt.Sprint(pid), "-n", "ip", "link", "set", "veth1", "up").Run(), "bring up veth1")
+
+    fmt.Println("-> Bringing up loopback")
+    must(exec.Command("nsenter", "-t", fmt.Sprint(pid), "-n", "ip", "link", "set", "lo", "up").Run(), "bring up lo")
+
+    fmt.Println("-> Adding default route")
+    must(exec.Command("nsenter", "-t", fmt.Sprint(pid), "-n", "ip", "route", "add", "default", "via", "10.0.0.1").Run(), "add default route")
+
+    fmt.Println("Network setup complete")
 }
-
 
 func child() {
     fmt.Printf("Running %v as PID %d\n", os.Args[2:], os.Getpid())
 
-    rootfs := "/home/chris/containers-from-scratch/rootfs"
+    // ✅ Set hostname inside UTS namespace
+    must(syscall.Sethostname([]byte("container")), "set hostname")
 
-    must(syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, ""))
+    rootfs := "/home/chris/containers-from-scratch/alpine-rootfs"
 
-    must(os.MkdirAll(filepath.Join(rootfs, "proc"), 0755))
-    must(os.MkdirAll(filepath.Join(rootfs, "old_root"), 0755))
+    must(syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, ""), "remount / as private")
 
-    // Make sure rootfs is a mount point
-    must(syscall.Mount(rootfs, rootfs, "", syscall.MS_BIND|syscall.MS_REC, ""))
+    must(os.MkdirAll(filepath.Join(rootfs, "proc"), 0755), "create /proc")
+    must(syscall.Mount("proc", filepath.Join(rootfs, "proc"), "proc", 0, ""), "mount proc")
 
-    must(syscall.Mount("proc", filepath.Join(rootfs, "proc"), "proc", 0, ""))
+    must(os.MkdirAll(filepath.Join(rootfs, "etc"), 0755), "mkdir /etc")
+    must(syscall.Mount("/etc/resolv.conf", filepath.Join(rootfs, "etc/resolv.conf"), "", syscall.MS_BIND, ""), "bind mount resolv.conf")
 
-    must(syscall.Chdir(rootfs))
-    must(syscall.PivotRoot(".", "old_root"))
-    must(syscall.Chdir("/"))
+    must(os.MkdirAll(filepath.Join(rootfs, "old_root"), 0755), "create old_root")
+    must(syscall.Mount(rootfs, rootfs, "", syscall.MS_BIND|syscall.MS_REC, ""), "bind mount new rootfs")
+    must(syscall.Chdir(rootfs), "chdir into new root")
+    must(syscall.PivotRoot(".", "old_root"), "pivot_root")
+    must(syscall.Chdir("/"), "chdir / after pivot")
 
-    must(syscall.Unmount("/old_root", syscall.MNT_DETACH))
-    must(os.Remove("/old_root"))
+    must(syscall.Unmount("/old_root", syscall.MNT_DETACH), "unmount old_root")
+    must(os.Remove("/old_root"), "remove old_root dir")
 
     cmd := exec.Command(os.Args[2], os.Args[3:]...)
     cmd.Stdin = os.Stdin
     cmd.Stdout = os.Stdout
     cmd.Stderr = os.Stderr
+    cmd.SysProcAttr = &syscall.SysProcAttr{
+        Credential: &syscall.Credential{Uid: 0, Gid: 0},
+    }
 
-    must(cmd.Run())
+    must(cmd.Run(), "run command inside container")
 }
 
-func must(err error) {
+func must(err error, msg string) {
     if err != nil {
-        panic(err)
+        log.Fatalf("ERROR [%s]: %v", msg, err)
     }
 }
